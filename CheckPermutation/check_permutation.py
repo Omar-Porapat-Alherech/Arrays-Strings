239846032208f22f78300639cb494dfc91e2e2f5
oa982 <oa82@cornell.edu>
2021-06-23 21:48:29 -0400

    Dictionary space saver sol

--- CheckPermutation/check_permutation.py
+++ CheckPermutation/check_permutation.py
@@ -16,8 +16,39 @@ def check_permutation_brute_force(string_a, string_b):
             return True
     return False
 
+
 # TODO Sort + compare
+
+
+
+
 # Space Efficient go through one and decrease dict going through other
+def check_permutation_one_dict(string_a, string_b):
+    if len(string_a) == len(string_b):
+        str_dict = {}
+        for letter in string_a:
+            if letter in str_dict:
+                str_dict[letter] += 1
+            else:
+                str_dict[letter] = 1
+        for letter in string_b:
+            if letter in str_dict:
+                if str_dict[letter] == 0:
+                    return False
+                else:
+                    str_dict[letter] -= 1
+                    if str_dict[letter] == 0:
+                        str_dict.pop(letter)
+            else:
+                return False
+        return True
+    return False
+
+
 if __name__ == '__main__':
     result = check_permutation_brute_force('aacde', 'abcda')
+    print(result)
+    result = check_permutation_brute_force('aacde', 'abda')
+    print(result)
+    result = check_permutation_one_dict('bcdaa', 'bbaad')
     print(result)
